90c64b0dde5fc93735d8cad1856641638fe1c0dc
Max <max.holmberg96@gmail.com>
2020-05-02 12:15:04 +0200

    The model compiles and from the summary looks like it will upscale images.

--- run.py
+++ run.py
@@ -1,26 +1,54 @@
 import tensorflow as tf
 
 
-def FSRCNN(input_shape, d, s, m):
+def FSRCNN(input_shape, d, s, m, upscaling):
     """
     No channel depth is set for the filters, this is due to the filters
     channels automatically being set to the same number of channels as the
     input. For more information see: https://stackoverflow.com/a/45055094/13185722
     So it is called Conv2D because it is sliding across two dimensions only.
     """
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(input_shape=input_shape, filters=d, kernel_size=5, strides=(1, 1), data_format="channels_last"),
-        tf.keras.layers.PReLU(),
-        tf.keras.layers.Conv2D(filters=s, kernel_size=1, strides=(1, 1)),
-        tf.keras.layers.PReLU(),
-    ])
+    model = tf.keras.models.Sequential()
+    """
+    The first convolution is the Feature Extraction which is denoted Conv(5, d, 1) in the paper. Channels = 1 is 
+    automatically set to one because the input has 1 channel.
+    """
+    model.add(tf.keras.layers.Conv2D(input_shape=input_shape, filters=d, kernel_size=5, strides=(1, 1), padding="same",
+                               data_format="channels_last"))
+    model.add(tf.keras.layers.PReLU())
+    """
+    The second convolution is the Shrinking which is denoted Conv(1, s, d) in the paper.
+    """
+    model.add(tf.keras.layers.Conv2D(filters=s, kernel_size=1, strides=(1, 1), padding="same"))
+    model.add(tf.keras.layers.PReLU())
 
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
+    """
+    The third part consists of m convolutional layers each denoted Conv(3, s, s) in the paper.
+    """
+    for i in range(m):
+        model.add(tf.keras.layers.Conv2D(filters=s, kernel_size=3, strides=(1, 1), padding="same"))
+        model.add(tf.keras.layers.PReLU())
+
+    """
+    The fourth part is the expanding part which is denoted Conv(1, d, s) in the paper. Note that this is the 
+    opposite of the shrinking, that is in the shrinking part we go from channels 56 -> 12 and here we go from 12 -> 56.
+    """
+    model.add(tf.keras.layers.Conv2D(filters=d, kernel_size=1, strides=(1, 1), padding="same"))
+    model.add(tf.keras.layers.PReLU())
+
+    """
+    The deconvolution part is denoted DeConv(9, 1, d) in the paper. The upscaling factor is decided by the stride.
+    Note that here we use Conv2DTranspose: https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv2DTranspose,
+    This layer is sometimes called Deconvolution.
+    """
+    model.add(tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=9, strides=(upscaling, upscaling), padding="same"))
+
+    model.compile(optimizer='sgd',
+                  loss='mean_squared_error',
+                  metrics=['mean_squared_error'])
     model.build()
     return model
-fsrcnn = FSRCNN(input_shape=(100, 100, 1), d=56, s=0, m=0)
+fsrcnn = FSRCNN(input_shape=(100, 100, 1), d=56, s=12, m=4, upscaling=3)
 fsrcnn.summary()
 #model.fit(x_train, y_train, epochs=5)
 #model.evaluate(x_test, y_test)
