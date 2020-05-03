7f890946135e1cc297c37e8aa55cba1033565201
Joel Lidin <joellidin@gmail.com>
2020-05-03 10:17:40 +0200

    Added rotation augmentation for the images

--- fsrcnn.py
+++ fsrcnn.py
@@ -8,12 +8,14 @@ Packages:
 
 import tensorflow as tf
 import pathlib
+import os
 from tqdm import tqdm
 import numpy as np
 
 MAX_PIXEL_VALUE = tf.constant(1.0)
 general100_path = "dataset/General-100/"
 image91_path = "dataset/T91/"
+
 
 def dataset_preparation(dataset, f_sub_lr, f_sub_hr, k, n):
     """
@@ -37,31 +39,42 @@ def dataset_preparation(dataset, f_sub_lr, f_sub_hr, k, n):
     x = []
     y = []
     data_dir = pathlib.Path(dataset)
-    for i in tqdm(data_dir.glob('*.bmp')):
+    _, extension = os.path.splitext(os.listdir(data_dir)[3])
+    for i in tqdm(data_dir.glob(f"*{extension}")):
         # Use grayscale because it is equivalent to first channel of yuv
-        img = tf.keras.preprocessing.image.load_img(str(i), color_mode='grayscale')
-        w, h = img.size
-        hr = tf.keras.preprocessing.image.img_to_array(img) / 255.0
-        new_w = int(w / n)
-        new_h = int(h / n)
-        lr = tf.image.resize(tf.identity(hr), (new_h, new_w), method=tf.image.ResizeMethod.BICUBIC)
-        lr_patches = tf.image.extract_patches(images=tf.expand_dims(lr, 0),
-                                              sizes=[1, f_sub_lr, f_sub_lr, 1],
-                                              strides=[1, k, k, 1],
-                                              rates=[1, 1, 1, 1],
-                                              padding='VALID')
-        hr_patches = tf.image.extract_patches(images=tf.expand_dims(hr, 0),
-                                              sizes=[1, f_sub_hr, f_sub_hr, 1],
-                                              strides=[1, k * n, k * n, 1],
-                                              rates=[1, 1, 1, 1],
-                                              padding='VALID')
-        for j in range(lr_patches.shape[1]):
-            for l in range(lr_patches.shape[2]):
-                lr_patch = tf.reshape(lr_patches[0, j, l], (1, f_sub_lr, f_sub_lr, 1))
-                hr_patch = tf.reshape(hr_patches[0, j, l], (1, f_sub_hr, f_sub_hr, 1))
-                x.append(lr_patch)
-                y.append(hr_patch)
-                #yield lr_patch, hr_patch, [None]
+        img = tf.keras.preprocessing.image.load_img(str(i), color_mode="grayscale")
+        for rot in range(4):
+            hr = tf.keras.preprocessing.image.img_to_array(img) / 255.0
+            hr = tf.image.rot90(hr, k=rot)
+            h, w, _ = hr.shape
+            new_w = int(w / n)
+            new_h = int(h / n)
+            lr = tf.image.resize(
+                tf.identity(hr), (new_h, new_w), method=tf.image.ResizeMethod.BICUBIC
+            )
+            lr_patches = tf.image.extract_patches(
+                images=tf.expand_dims(lr, 0),
+                sizes=[1, f_sub_lr, f_sub_lr, 1],
+                strides=[1, k, k, 1],
+                rates=[1, 1, 1, 1],
+                padding="VALID",
+            )
+            hr_patches = tf.image.extract_patches(
+                images=tf.expand_dims(hr, 0),
+                sizes=[1, f_sub_hr, f_sub_hr, 1],
+                strides=[1, k * n, k * n, 1],
+                rates=[1, 1, 1, 1],
+                padding="VALID",
+            )
+            for j in range(lr_patches.shape[1]):
+                for l in range(lr_patches.shape[2]):
+                    lr_patch = tf.reshape(lr_patches[0, j, l], (1, f_sub_lr, f_sub_lr, 1))
+                    lr_patch = tf.image.rot90(lr_patch, k=rot)
+                    hr_patch = tf.reshape(hr_patches[0, j, l], (1, f_sub_hr, f_sub_hr, 1))
+                    hr_patch = tf.image.rot90(hr_patch, k=rot)
+                    x.append(lr_patch)
+                    y.append(hr_patch)
+                # yield lr_patch, hr_patch, [None]
 
     return tf.concat(x, axis=0), tf.concat(y, axis=0)
 
@@ -82,34 +95,63 @@ def FSRCNN(input_shape, d, s, m, upscaling):
     """
     model = tf.keras.models.Sequential()
     """
-    The first convolution is the Feature Extraction which is denoted Conv(5, d, 1) in the paper. Channels = 1 is 
+    The first convolution is the Feature Extraction which is denoted Conv(5, d, 1) in the paper. Channels = 1 is
     automatically set to one because the input has 1 channel.
     """
-    model.add(tf.keras.layers.Conv2D(input_shape=input_shape, filters=d, kernel_size=5, padding="same",
-                                     data_format="channels_last", use_bias=False,
-                                     kernel_initializer=tf.keras.initializers.he_normal()))
+    model.add(
+        tf.keras.layers.Conv2D(
+            input_shape=input_shape,
+            filters=d,
+            kernel_size=5,
+            padding="same",
+            data_format="channels_last",
+            use_bias=False,
+            kernel_initializer=tf.keras.initializers.he_normal(),
+        )
+    )
     model.add(tf.keras.layers.PReLU())
     """
     The second convolution is the Shrinking which is denoted Conv(1, s, d) in the paper.
     """
-    model.add(tf.keras.layers.Conv2D(filters=s, kernel_size=1, padding="same", use_bias=False,
-                                     kernel_initializer=tf.keras.initializers.he_normal()))
+    model.add(
+        tf.keras.layers.Conv2D(
+            filters=s,
+            kernel_size=1,
+            padding="same",
+            use_bias=False,
+            kernel_initializer=tf.keras.initializers.he_normal(),
+        )
+    )
     model.add(tf.keras.layers.PReLU())
 
     """
     The third part consists of m convolutional layers each denoted Conv(3, s, s) in the paper.
     """
     for i in range(m):
-        model.add(tf.keras.layers.Conv2D(filters=s, kernel_size=3, padding="same", use_bias=False,
-                                         kernel_initializer=tf.keras.initializers.he_normal()))
+        model.add(
+            tf.keras.layers.Conv2D(
+                filters=s,
+                kernel_size=3,
+                padding="same",
+                use_bias=False,
+                kernel_initializer=tf.keras.initializers.he_normal(),
+            )
+        )
         model.add(tf.keras.layers.PReLU())
 
     """
-    The fourth part is the expanding part which is denoted Conv(1, d, s) in the paper. Note that this is the 
+    The fourth part is the expanding part which is denoted Conv(1, d, s) in the paper. Note that this is the
     opposite of the shrinking, that is in the shrinking part we go from channels 56 -> 12 and here we go from 12 -> 56.
     """
-    model.add(tf.keras.layers.Conv2D(filters=d, kernel_size=1, padding="same", use_bias=False,
-                                     kernel_initializer=tf.keras.initializers.he_normal()))
+    model.add(
+        tf.keras.layers.Conv2D(
+            filters=d,
+            kernel_size=1,
+            padding="same",
+            use_bias=False,
+            kernel_initializer=tf.keras.initializers.he_normal(),
+        )
+    )
     model.add(tf.keras.layers.PReLU())
 
     """
@@ -117,23 +159,29 @@ def FSRCNN(input_shape, d, s, m, upscaling):
     Note that here we use Conv2DTranspose: https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv2DTranspose,
     This layer is sometimes called Deconvolution.
     """
-    model.add(tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=9, strides=(upscaling, upscaling), padding="same",
-                                              use_bias=False,
-                                              kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.001)))
+    model.add(
+        tf.keras.layers.Conv2DTranspose(
+            filters=1,
+            kernel_size=9,
+            strides=(upscaling, upscaling),
+            padding="same",
+            use_bias=False,
+            kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.001),
+        )
+    )
 
     sgd = tf.keras.optimizers.SGD(learning_rate=1e-2, momentum=0.0)
-    model.compile(optimizer=sgd,
-                  loss='mean_squared_error',
-                  metrics=['mean_squared_error', psnr])
+    model.compile(optimizer=sgd, loss="mean_squared_error", metrics=["mean_squared_error", psnr])
     model.build()
     return model
+
 
 upscaling = 3
 f_sub_lr = 40
 f_sub_hr = f_sub_lr * upscaling
 patch_stride = 4
 fsrcnn = FSRCNN(input_shape=(f_sub_lr, f_sub_lr, 1), d=56, s=12, m=4, upscaling=upscaling)
-#fsrcnn = FSRCNN(input_shape=(f_sub_lr, f_sub_lr, 1), d=32, s=5, m=1, upscaling=upscaling)
+# fsrcnn = FSRCNN(input_shape=(f_sub_lr, f_sub_lr, 1), d=32, s=5, m=1, upscaling=upscaling)
 
 fsrcnn.summary()
 param_count = 0
@@ -142,15 +190,17 @@ for i in range(0, len(fsrcnn.layers), 2):
 print("Number of parameters (PReLU not included):", param_count)
 
 # This is not used I think, as we use here f_sub_lr = a and then f_sub_hr = a * upscaling
-#Upscaling factor: 2x = f_sub_lr=10, f_sub_hr=19
-#Upscaling factor: 3x = f_sub_lr=7, f_sub_hr=19
-#Upscaling factor: 4x = f_sub_lr=6, f_sub_hr=21
+# Upscaling factor: 2x = f_sub_lr=10, f_sub_hr=19
+# Upscaling factor: 3x = f_sub_lr=7, f_sub_hr=19
+# Upscaling factor: 4x = f_sub_lr=6, f_sub_hr=21
 
-x, y = dataset_preparation(general100_path, f_sub_lr=f_sub_lr, f_sub_hr=f_sub_hr, k=patch_stride, n=upscaling)
-#np.savez("data", x=x, y=y)
-#d = np.load("data.npz")
-#x = d["x"]
-#y = d["y"]
+x, y = dataset_preparation(
+    general100_path, f_sub_lr=f_sub_lr, f_sub_hr=f_sub_hr, k=patch_stride, n=upscaling
+)
+# np.savez("data", x=x, y=y)
+# d = np.load("data.npz")
+# x = d["x"]
+# y = d["y"]
 print("max", np.max(x))
 print("x.shape", x.shape)
 print("y.shape", y.shape)
